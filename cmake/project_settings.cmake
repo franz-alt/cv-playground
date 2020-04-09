2e8555e965b32e46c3d1455b371fb3c874a37e3d
Franz Alt <franz.s.alt@gmail.com>
2020-04-09 12:00:25 +0200

    Added prefix 'd' in case of debug build type to all binaries

--- cmake/project_settings.cmake
+++ cmake/project_settings.cmake
@@ -4,3 +4,9 @@
 if(NOT CMAKE_BUILD_TYPE)
   set(CMAKE_BUILD_TYPE Release)
 endif()
+
+string(TOUPPER ${CMAKE_BUILD_TYPE} BUILDTYPE)
+
+if("${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
+    set(TARGET_PREFIX "d")
+endif()
