584ba3c9921c70ab510f735ef85bddf23d10c7ba
Franz Alt <franz.s.alt@gmail.com>
2020-04-09 09:56:38 +0200

    Used 'Release' as default build type

---
+++ cmake/project_settings.cmake
@@ -0,0 +1,6 @@
+# common project header to be used in root project files before or right after the 'project()' command
+
+# set the default build type to release
+if(NOT CMAKE_BUILD_TYPE)
+  set(CMAKE_BUILD_TYPE Release)
+endif()
