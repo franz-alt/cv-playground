cb15d9a59d7eda18e9c9c36cc28a5e0d6efc6799
Franz Alt <franz.s.alt@gmail.com>
2020-05-09 18:17:13 +0200

    Added installation directives to CMake files

--- cmake/project_settings.cmake
+++ cmake/project_settings.cmake
@@ -10,3 +10,17 @@ string(TOUPPER ${CMAKE_BUILD_TYPE} BUILDTYPE)
 if("${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
     set(TARGET_PREFIX "d")
 endif()
+
+# set global library folder
+if(NOT LIBRARY_FOLDER)
+    set(LIBRARY_FOLDER ${PROJECT_NAME_LOWERCASE})
+endif()
+
+# installation directory of libraries
+set(INSTALL_LIB_DIR lib CACHE PATH "Relative installation path for libraries")
+
+# installation directory of binaries
+set(INSTALL_BIN_DIR bin CACHE PATH "Relative installation path for executables")
+
+# installation directory of headers
+set(INSTALL_INCLUDE_DIR include CACHE PATH "Relative installation path for header files")
