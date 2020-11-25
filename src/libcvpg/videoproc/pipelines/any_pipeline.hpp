4f98911e8969f6fb40368788e95b15c1c9feb191
Franz Alt <franz.s.alt@gmail.com>
2020-11-25 21:07:00 +0100

    [libcvpg] Fix amount of parameters of function 'start' at pipeline concept

--- src/libcvpg/videoproc/pipelines/any_pipeline.hpp
+++ src/libcvpg/videoproc/pipelines/any_pipeline.hpp
@@ -17,13 +17,13 @@
 
 #include <libcvpg/videoproc/pipelines/parameters.hpp>
 
-BOOST_TYPE_ERASURE_MEMBER((cvpg::videoproc::pipelines)(pipeline_has_start), start, 1);
+BOOST_TYPE_ERASURE_MEMBER((cvpg::videoproc::pipelines)(pipeline_has_start), start, 3);
 
 namespace cvpg::videoproc::pipelines {
 
 typedef boost::mpl::vector5<
     boost::asynchronous::pointer<>,
-    boost::type_erasure::same_type<boost::asynchronous::pointer<>::element_type,boost::type_erasure::_a>,
+    boost::type_erasure::same_type<boost::asynchronous::pointer<>::element_type, boost::type_erasure::_a>,
     boost::type_erasure::relaxed,
     boost::type_erasure::copy_constructible<>,
     pipeline_has_start<void(parameters::uris, parameters::scripts, parameters::callbacks), boost::type_erasure::_a>
